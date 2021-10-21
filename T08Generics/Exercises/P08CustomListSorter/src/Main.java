392328a4cf92dc2c05f5fd8ca0db5f749c82a659
Georgi Manev <georgi.rm@gmail.com>
2021-10-22 01:34:08 +0300

    Problem 8 "Custom List Sorter" from Exercises for Generics

---
+++ T08Generics/Exercises/P08CustomListSorter/src/Main.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        CustomList<String> myList = new CustomList<>();
+
+        String input = scanner.nextLine();
+        while (!input.equals("END")) {
+            String[] commandParameters = input.split("\\s+");
+            String command = commandParameters[0];
+
+            switch (command) {
+                case "Add":
+                    String elementToAdd = commandParameters[1];
+                    myList.add(elementToAdd);
+                    break;
+                case "Remove":
+                    int indexToRemove = Integer.parseInt(commandParameters[1]);
+                    myList.remove(indexToRemove);
+                    break;
+                case "Contains":
+                    String elementToSearch = commandParameters[1];
+                    System.out.println(myList.contains(elementToSearch));
+                    break;
+                case "Swap":
+                    int indexOfFirstElement = Integer.parseInt(commandParameters[1]);
+                    int indexOfSecondElement = Integer.parseInt(commandParameters[2]);
+                    myList.swap(indexOfFirstElement, indexOfSecondElement);
+                    break;
+                case "Greater":
+                    String elementToCompare = commandParameters[1];
+                    System.out.println(myList.countGreaterThan(elementToCompare));
+                    break;
+                case "Max":
+                    System.out.println(myList.getMax());
+                    break;
+                case "Min":
+                    System.out.println(myList.getMin());
+                    break;
+                case "Print":
+                    System.out.print(myList);
+                    break;
+                case "Sort":
+                    Sorter.sort(myList);
+                    break;
+            }
+
+            input = scanner.nextLine();
+        }
+    }
+}
