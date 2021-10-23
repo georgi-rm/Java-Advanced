95ee26d4979ae6db4ec8a65896739493891635fb
Georgi Manev <georgi.rm@gmail.com>
2021-10-23 23:57:28 +0300

    Problem 8 "Pet Clinics" from Exercises for Iterators and Comparators

---
+++ T09IteratorsAndComparators/Exercises/P08PetClinics/src/Pet.java
@@ -0,0 +1,23 @@
+public class Pet {
+    private final String name;
+    private final int age;
+    private final String kind;
+
+    public Pet(String name, int age, String kind) {
+        this.name = name;
+        this.age = age;
+        this.kind = kind;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+}
